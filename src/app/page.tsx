289f4e953c53384dd5f14788bd1dc0b1baf60bcb
Harsh Pathak <harshmis2001@gmail.com>
2025-06-23 11:52:37 +0000

    Always back camera enable not front

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
       }
 
       try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } });
         if (videoRef.current) {
           videoRef.current.srcObject = stream;
         }
