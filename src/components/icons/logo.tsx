68e6207092b986b0430aad1397c11a1d89170eda
Harsh Pathak <harshmis2001@gmail.com>
2025-06-25 06:13:49 +0000

    Not upload any logo image in the card

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -1,23 +1,3 @@
-import Image from 'next/image';
-import { cn } from '@/lib/utils';
-
 export function Logo({className}: {className?: string}) {
-  return (
-    <div
-      className={cn(
-        'flex flex-col items-center justify-center',
-        className
-      )}
-    >
-      <div className="relative h-80 w-80">
-        <Image
-          src="/logo-light.png"
-          alt="AI Question Solver Logo"
-          fill
-          className="object-contain"
-          priority
-        />
-      </div>
-    </div>
-  );
+  return null;
 }
