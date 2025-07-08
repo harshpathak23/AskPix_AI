365812abe199fdc50cc6407c83bd0dfa63238f00
Harsh Pathak <harshmis2001@gmail.com>
2025-07-08 00:25:19 +0000

    I created assets folder and upload icon.png file now what next

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,7 +6,22 @@ const config: CapacitorConfig = {
   webDir: 'out',
   server: {
     androidScheme: 'https'
-  }
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: "#EEEEF2",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#4F3A93",
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
+  },
 };
 
 export default config;
